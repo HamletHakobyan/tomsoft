c382d6bb848a0501aab5a45a4338ce1a65f69f85
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2009-01-12 18:00:45 +0000

    Initial commit

---
+++ ImageShackUploader/SmtpAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageShackUploader
+{
+    public class SmtpAccount
+    {
+        public SmtpAccount()
+        {
+            this.Port = 25;
+        }
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSSL { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
