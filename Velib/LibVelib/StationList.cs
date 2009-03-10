a7fc9db5634f447191a071857504a730f1720d10
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2009-03-10 18:21:04 +0000

    Initial commit

---
+++ Velib/LibVelib/StationList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LibVelib
+{
+    [XmlRoot("carto")]
+    public class StationList
+    {
+        [XmlArray("markers")]
+        [XmlArrayItem("marker")]
+        public List<Station> Stations { get; set; }
+
+        [XmlArray("arrondissements")]
+        [XmlArrayItem("arrondissement")]
+        public List<Area> Areas { get; set; }
+    }
+}
