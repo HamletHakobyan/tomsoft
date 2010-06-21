85532e06bb9fc8bf69f668d605d084fd5a8b19b4
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2010-06-21 15:25:10 +0000

    Added Target property to PatternExclusion

--- PkgMaker/PkgMaker.Core/PatternExclusion.cs
+++ PkgMaker/PkgMaker.Core/PatternExclusion.cs
@@ -11,10 +11,26 @@ namespace PkgMaker.Core
         [XmlText]
         public string Pattern { get; set; }
 
+        [XmlAttribute]
+        public ExclusionTarget Target { get; set; }
+
         public override bool IsMatch(FileSystemInfo item, string basePath)
         {
+            bool isDirectory = item is DirectoryInfo;
+            if (Target == ExclusionTarget.File && isDirectory)
+                return false;
+            if (Target == ExclusionTarget.Directory && !isDirectory)
+                return false;
+
             string relativePath = PathUtil.GetRelativePath(basePath, item.FullName);
             return Regex.IsMatch(relativePath, this.Pattern, RegexOptions.IgnoreCase);
         }
+    }
+
+    public enum ExclusionTarget
+    {
+        Both,
+        File,
+        Directory
     }
 }
