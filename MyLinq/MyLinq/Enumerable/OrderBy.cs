0528f569401f609d311521727635fef337a84513
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-01-05 22:31:48 +0000

    Fini OrderBy (à améliorer)

---
+++ MyLinq/MyLinq/Enumerable/OrderBy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Linq = System.Linq;
+
+namespace MyLinq
+{
+    public static partial class Enumerable
+    {
+        public static Linq.IOrderedEnumerable<TSource> OrderBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.OrderByCore(keySelector, null, false);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> OrderBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            return source.OrderByCore(keySelector, comparer, false);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> OrderByDescending<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.OrderByCore(keySelector, null, true);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> OrderByDescending<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            return source.OrderByCore(keySelector, comparer, true);
+        }
+
+        private static Linq.IOrderedEnumerable<TSource> OrderByCore<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer,
+            bool descending)
+        {
+            source.CheckArgumentNotNull("source");
+            keySelector.CheckArgumentNotNull("keySelector");
+            
+            var elementComparer = OrderedEnumerable<TSource>.CreateComparer<TKey>(keySelector, comparer, descending);
+
+            return new OrderedEnumerable<TSource>(source, elementComparer);
+        }
+    }
+}
---
+++ MyLinq/MyLinq/Enumerable/ThenBy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Linq = System.Linq;
+
+namespace MyLinq
+{
+    public static partial class Enumerable
+    {
+        public static Linq.IOrderedEnumerable<TSource> ThenBy<TSource, TKey>(
+            this Linq.IOrderedEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.ThenByCore(keySelector, null, false);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> ThenBy<TSource, TKey>(
+            this Linq.IOrderedEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            return source.ThenByCore(keySelector, comparer, false);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> ThenByDescending<TSource, TKey>(
+            this Linq.IOrderedEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return source.ThenByCore(keySelector, null, true);
+        }
+
+        public static Linq.IOrderedEnumerable<TSource> ThenByDescending<TSource, TKey>(
+            this Linq.IOrderedEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            return source.ThenByCore(keySelector, comparer, true);
+        }
+
+        private static Linq.IOrderedEnumerable<TSource> ThenByCore<TSource, TKey>(
+            this Linq.IOrderedEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer,
+            bool descending)
+        {
+            return source.CreateOrderedEnumerable(keySelector, comparer, descending);
+        }
+    }
+}
