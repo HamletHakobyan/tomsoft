8293913ad0e443a8bdefb48445be11c02b6b9ca2
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-01-05 22:31:48 +0000

    Fini OrderBy (à améliorer)

--- MyLinq/MyLinq/OrderedEnumerable.cs
+++ MyLinq/MyLinq/OrderedEnumerable.cs
@@ -1,28 +1,54 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Linq = System.Linq;
 
 namespace MyLinq
 {
-    class OrderedEnumerable<TElement> : IOrderedEnumerable<TElement>
+    class OrderedEnumerable<TElement> : Linq.IOrderedEnumerable<TElement>
     {
         private readonly IEnumerable<TElement> _source;
+        private readonly IComparer<TElement> _comparer;
 
-        public OrderedEnumerable(IEnumerable<TElement> source)
+        public OrderedEnumerable(IEnumerable<TElement> source, IComparer<TElement> comparer)
         {
             _source = source;
+            _comparer = comparer;
         }
 
-        public virtual IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        public Linq.IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
         {
-            return new OrderedEnumerable<TElement, TKey>(_source, keySelector, comparer, descending);
+            keySelector.CheckArgumentNotNull("keySelector");
+
+            var secondComparer = CreateComparer(keySelector, comparer, descending);
+            var compoundComparer = new CompoundComparer<TElement>(_comparer, secondComparer);
+            return new OrderedEnumerable<TElement>(_source, compoundComparer);
         }
 
-        public virtual IEnumerator<TElement> GetEnumerator()
+        public IEnumerator<TElement> GetEnumerator()
         {
-            return _source.GetEnumerator();
+            // TODO: to be improved
+            var list = _source.Select((x, i) => new IndexedElement(x, i)).ToList();
+            var elementComparer = new ProjectionComparer<IndexedElement, TElement>(i => i.Element, _comparer);
+            var indexComparer = new ProjectionComparer<IndexedElement, int>(i => i.Index, null);
+            var actualComparer = new CompoundComparer<IndexedElement>(elementComparer, indexComparer);
+            list.Sort(actualComparer);
+            return list.Select(i => i.Element).GetEnumerator();
+        }
+
+        private class IndexedElement
+        {
+            private readonly TElement _element;
+            private readonly int _index;
+
+            public IndexedElement(TElement element, int index)
+            {
+                _element = element;
+                _index = index;
+            }
+
+            public int Index { get { return _index; } }
+            public TElement Element { get { return _element; } }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -30,43 +56,11 @@ namespace MyLinq
             return GetEnumerator();
         }
 
-        protected IEnumerable<TElement> Source
-        {
-            get { return _source; }
-        }
-    }
-
-    class OrderedEnumerable<TElement, TFirstKey> : OrderedEnumerable<TElement>
-    {
-        private readonly IComparer<TElement> _comparer;
-        private readonly Func<TElement, TFirstKey> _keySelector;
-        private readonly IComparer<TFirstKey> _keyComparer;
-
-        public OrderedEnumerable(IEnumerable<TElement> source, Func<TElement, TFirstKey> keySelector, IComparer<TFirstKey> keyComparer, bool descending)
-            : base(source)
-        {
-            _comparer = CreateComparer(keySelector, keyComparer, descending);
-        }
-
-        private static IComparer<TElement> CreateComparer<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> keyComparer, bool descending)
+        internal static IComparer<TElement> CreateComparer<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> keyComparer, bool descending)
         {
             if (descending)
                 keyComparer = new ReverseComparer<TKey>(keyComparer);
             return new ProjectionComparer<TElement, TKey>(keySelector, keyComparer);
-        }
-
-        public override IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
-        {
-            //TODO
-            throw new NotImplementedException();
-            return base.CreateOrderedEnumerable<TKey>(keySelector, comparer, descending);
-        }
-
-        public override IEnumerator<TElement> GetEnumerator()
-        {
-            var list = Source.ToList();
-            list.Sort(_comparer);
-            return list.GetEnumerator();
         }
     }
 }
