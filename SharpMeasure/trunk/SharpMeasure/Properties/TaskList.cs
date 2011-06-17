d607d6b4cc03cbc60ed040b1233bbca8f13ad5e1
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-06-17 00:13:34 +0000

    Reduction of composite units

---
+++ SharpMeasure/trunk/SharpMeasure/Properties/TaskList.cs
@@ -0,0 +1,3 @@
+// TODO : Reduction of composite units (m/m/s => s) (partially done, TBT)
+// ENH : Static checking of unit compatibility (possible ?)
+// TODO : Handling of division/multiplication of units of same quantity
