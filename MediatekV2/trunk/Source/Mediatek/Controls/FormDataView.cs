c434259ec79e43717c27caeb7a275122c9852c28
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-07-28 01:26:51 +0000

    Created FormDataView, still working on it

---
+++ MediatekV2/trunk/Source/Mediatek/Controls/FormDataView.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Mediatek.Controls
+{
+    public class FormDataView : ItemsControl
+    {
+        static FormDataView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FormDataView),
+                                                     new FrameworkPropertyMetadata(typeof(FormDataView)));
+        }
+
+        public bool IsInEditMode
+        {
+            get { return (bool)GetValue(IsInEditModeProperty); }
+            set { SetValue(IsInEditModeProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsInEditModeProperty =
+            DependencyProperty.Register("IsInEditMode", typeof(bool), typeof(FormDataView), new UIPropertyMetadata(false));
+    }
+}
