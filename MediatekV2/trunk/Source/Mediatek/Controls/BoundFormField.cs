536f5d2e5ccf34209b502eb9582492386d0c0ebd
tom103@free.fr <tom103@free.fr@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-07-28 01:26:51 +0000

    Created FormDataView, still working on it

---
+++ MediatekV2/trunk/Source/Mediatek/Controls/BoundFormField.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Mediatek.Controls
+{
+    public abstract class BoundFormField : FormField
+    {
+        private BindingBase _binding;
+        public BindingBase Binding
+        {
+            get { return _binding; }
+            set
+            {
+                _binding = value;
+                SetBinding(ValueProperty, value);
+            }
+        }
+
+        public object Value
+        {
+            get { return GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value",
+                typeof(object),
+                typeof(TextFormField),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        //public override void OnApplyTemplate()
+        //{
+        //    base.OnApplyTemplate();
+        //    var display = Template.FindName("PART_Display", this) as FrameworkElement;
+        //    var editor = Template.FindName("PART_Editor", this) as FrameworkElement;
+        //    if (this.Binding != null)
+        //    {
+        //        if (display != null)
+        //            display.SetBinding(DataContextProperty, this.Binding);
+        //        if (editor != null)
+        //            editor.SetBinding(DataContextProperty, this.Binding); 
+        //    }
+        //}
+    }
+
+    public class TextFormField : BoundFormField
+    {
+        static TextFormField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(TextFormField),
+                new FrameworkPropertyMetadata(typeof(TextFormField)));
+        }
+    }
+
+    public class CheckBoxFormField : BoundFormField
+    {
+        static CheckBoxFormField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxFormField),
+                                                     new FrameworkPropertyMetadata(typeof(CheckBoxFormField)));
+        }
+
+        public bool IsThreeState
+        {
+            get { return (bool)GetValue(IsThreeStateProperty); }
+            set { SetValue(IsThreeStateProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsThreeStateProperty =
+            DependencyProperty.Register("IsThreeState", typeof(bool), typeof(CheckBoxFormField), new UIPropertyMetadata(false));
+
+    }
+
+    public class ComboBoxFormField : BoundFormField
+    {
+        static ComboBoxFormField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ComboBoxFormField),
+                                                     new FrameworkPropertyMetadata(typeof(ComboBoxFormField)));
+        }
+    }
+
+    public class RatingFormField : BoundFormField
+    {
+        static RatingFormField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RatingFormField),
+                                                     new FrameworkPropertyMetadata(typeof(RatingFormField)));
+        }
+    }
+}
---
+++ MediatekV2/trunk/Source/Mediatek/Controls/FormField.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Mediatek.Controls
+{
+    [TemplatePart(Name = "PART_Display", Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = "PART_Editor", Type = typeof(FrameworkElement))]
+    public class FormField : Control
+    {
+        static FormField()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FormField),
+                                                     new FrameworkPropertyMetadata(typeof(FormField)));
+        }
+
+        public object MyStyleKey
+        {
+            get { return this.DefaultStyleKey; }
+        }
+
+            public object Header
+        {
+            get { return GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(object), typeof(FormField), new UIPropertyMetadata(null));
+
+        public DataTemplate EditorTemplate
+        {
+            get { return (DataTemplate)GetValue(EditorTemplateProperty); }
+            set { SetValue(EditorTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty EditorTemplateProperty =
+            DependencyProperty.Register("EditorTemplate", typeof(DataTemplate), typeof(FormField), new UIPropertyMetadata(null));
+
+        public DataTemplate DisplayTemplate
+        {
+            get { return (DataTemplate)GetValue(DisplayTemplateProperty); }
+            set { SetValue(DisplayTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisplayTemplateProperty =
+            DependencyProperty.Register("DisplayTemplate", typeof(DataTemplate), typeof(FormField), new UIPropertyMetadata(null));
+    }
+}
