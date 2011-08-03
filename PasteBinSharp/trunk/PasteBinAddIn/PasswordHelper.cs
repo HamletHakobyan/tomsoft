aff7f017f463ab0e3b7c82cc7e52fc755c4202fe
thomas.levesque@gmail.com <thomas.levesque@gmail.com@938a434c-eb50-0410-84e0-e5c8a7cfc46c>
2011-08-03 15:51:01 +0000

    PasteBinAddIn presque fini

---
+++ PasteBinSharp/trunk/PasteBinAddIn/PasswordHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasteBinAddIn
+{
+    static class PasswordHelper
+    {
+        public static string UnprotectPassword(string password)
+        {
+            byte[] protectedBytes = Convert.FromBase64String(password);
+            byte[] bytes = ProtectedData.Unprotect(protectedBytes, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string ProtectPassword(string password)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+            byte[] protectedBytes = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(protectedBytes);
+        }
+    }
+}
---
+++ PasteBinSharp/trunk/PasteBinAddIn/SettingsWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PasteBinAddIn.Properties;
+
+namespace PasteBinAddIn
+{
+    /// <summary>
+    /// Logique d'interaction pour SettingsWindow.xaml
+    /// </summary>
+    partial class SettingsWindow : Window
+    {
+        private readonly Settings _settings;
+
+        public SettingsWindow(Settings settings, string errorMessage = null)
+        {
+            _settings = settings;
+            
+            InitializeComponent();
+
+            txtApiDevKey.Text = _settings.ApiDevKey;
+            txtUserName.Text = _settings.UserName;
+            pwdPassword.Password = string.Empty;
+            if (!string.IsNullOrEmpty(_settings.Password))
+                pwdPassword.Password = PasswordHelper.UnprotectPassword(_settings.Password);
+            lblError.Content = errorMessage;
+            imgError.Visibility = lblError.Visibility = string.IsNullOrEmpty(errorMessage) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            _settings.ApiDevKey = txtApiDevKey.Text;
+            _settings.UserName = txtUserName.Text;
+            _settings.Password = string.Empty;
+            if (!string.IsNullOrEmpty(pwdPassword.Password))
+                _settings.Password = PasswordHelper.ProtectPassword(pwdPassword.Password);
+            _settings.Save();
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
